df544054d09b2906a8c0bdada427acf2b34f339d
andreivercosa <andreivercosa@gmail.com>
2019-03-12 21:26:12 -0400

    v1

---
+++ TabPageMaster/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TabPageMaster
+{
+    public partial class MainPage : ContentPage
+    {
+        void concat(object sender, System.EventArgs e)
+        {
+            textComplet.Text = Part1.Text +" "+ Part2.Text;
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TabPageMaster/SecondPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TabPageMaster
+{
+    public partial class SecondPage : ContentPage
+    {
+        public SecondPage()
+        {
+            InitializeComponent();
+        }
+
+        void valor_slider(object sender, Xamarin.Forms.ValueChangedEventArgs e)
+        {
+            valor.Text = Math.Ceiling(bola.Value).ToString();
+        }
+    }
+}
